ad35a592e4333bea399a183b559ebc61700529b2
Nicholas Bradley <nbradley@uw.edu>
2024-04-22 11:39:42 -0700

    added run.sh and log files

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+test -e logs/latest && rm -r logs/latest
+mkdir logs/latest
+
+mem_arg='$(expr {resources.mem_mb} / {threads})'
+out_path=$(realpath ./logs/latest/)
+err_path=$(realpath ./logs/latest/)
+#dep_list='"$(sed '\''s/ /,/g'\'' <<< '\''{dependencies}'\'' )"'
+
+cluster_cmd="qsub -P shendure_fowler -terse -l mfree=${mem_arg}M -l h_rt=48:0:0 -l 'hostname=!s022&!s025' -o $out_path -e $err_path -pe serial {threads}"
+#cluster_cmd="qsub -terse -l mfree=${mem_arg}M -l h_rt=48:0:0 -l 'hostname=!s022&!s025' -o $out_path -e $err_path -pe serial {threads}"
+
+snakemake \
+    --cluster "$cluster_cmd" \
+    --cluster-cancel "qdel" \
+    -j 128 \
+    $* --cores 50 --resources mem_mb=1500000 --set-resource-scopes mem_mb=global threads=global \
+    --rerun-triggers mtime params input software-env \
+    --keep-incomplete
+    #--immediate-submit --notemp \
+
+cd logs
+timestamp=$(date +%Y-%m-%d-%H-%M-%S)
+mv latest/ $timestamp/
+tar -czf ${timestamp}.tar.gz $timestamp/
+mv $timestamp/ latest/
+cd ..
+
