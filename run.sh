13f4f76ee135a6d648d62e05bd067398893daef5
Nicholas Bradley <nbradley@uw.edu>
2024-11-11 22:08:25 -0800

    separated segmentations and made possible running on gpu

--- run.sh
+++ run.sh
@@ -12,10 +12,12 @@ mem_arg='$(expr {resources.mem_mb} / {threads})'
 out_path='$( (test -f logs/{output[0]}.out && rm logs/{output[0]}.out); mkdir -p $(dirname logs/{output[0]}.out); realpath logs/{output[0]}.out)'
 err_path='$( (test -f logs/{output[0]}.err && rm logs/{output[0]}.err); mkdir -p $(dirname logs/{output[0]}.err); realpath logs/{output[0]}.err)'
 #dep_list='"$(sed '\''s/ /,/g'\'' <<< '\''{dependencies}'\'' )"'
+cuda=
+#cuda='$(test "{resources.cuda}" -eq 1 && echo -l cuda=1)'
 
 #cluster_cmd="qsub -P shendure_fowler -terse -l mfree=${mem_arg}M -l h_rt=48:0:0 -l 'hostname=!s022&!s025' -o $out_path -e $err_path -pe serial {threads}"
 #cluster_cmd="echo {log}"
-cluster_cmd="qsub -terse -l mfree=${mem_arg}M -l h_rt=48:0:0 -l h=fl004 -o $out_path -e $err_path -pe serial {threads}"
+cluster_cmd="qsub -terse -l mfree=${mem_arg}M -l h_rt=48:0:0 -l h=fl004 -o $out_path -e $err_path $cuda -pe serial {threads}"
 
 configfile=
 if test -f config.yaml; then
@@ -31,6 +33,7 @@ snakemake \
     -j 128 \
     $* --cores 50 --resources mem_mb=2000000 --set-resource-scopes mem_mb=global threads=global \
     --rerun-triggers mtime params input software-env \
-    --keep-incomplete --latency-wait 120 $configfile
+    --keep-incomplete --latency-wait 120 $configfile \
+    --default-resources cuda=0
     #--immediate-submit --notemp \
 
